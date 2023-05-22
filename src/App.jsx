2132859eb558cb097f70118f6b24c315a512ac17
[Skaut88] <[skaboy69@gmail.com]>
2023-05-22 16:08:10 +0300

    add componets, add routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,18 @@
+import { NavLink, Route, Routes } from "react-router-dom";
 import "./App.css";
+
+const Profile = () => {
+  return <h1>Эта страница с профилем</h1>;
+};
+const Messages = () => {
+  return <h1>Эта страница с сообщениями</h1>;
+};
+const Settings = () => {
+  return <h1>Эта страница с настройками</h1>;
+};
+const Shop = () => {
+  return <h1>Магазин</h1>;
+};
 
 function App() {
   return (
@@ -6,21 +20,34 @@ function App() {
       <div className="row mt-3">
         <div className="col-md-3">
           <div className="nav flex-column nav-pills">
-            <a className="nav-link" href="profile">
+            <NavLink className="nav-link" to="profile">
               Профиль
-            </a>
-            <a className="nav-link" href="messages">
+            </NavLink>
+            <NavLink className="nav-link" to="messages">
               Сообщения
-            </a>
-            <a className="nav-link" href="settings">
+            </NavLink>
+            <NavLink className="nav-link" to="settings">
               Настройки
-            </a>
-            <a className="nav-link" href="shop">
+            </NavLink>
+            <NavLink className="nav-link" to="shop">
               Магазин
-            </a>
+            </NavLink>
           </div>
         </div>
-        <div className="col-md-9"></div>
+        <div className="col-md-9">
+          <Routes>
+            <Route path="/profile" element={<Profile />} />
+          </Routes>
+          <Routes>
+            <Route path="/messages" element={<Messages />} />
+          </Routes>
+          <Routes>
+            <Route path="/settings" element={<Settings />} />
+          </Routes>
+          <Routes>
+            <Route path="/shop" element={<Shop />} />
+          </Routes>
+        </div>
       </div>
     </div>
   );
