ad86e84fe53e8a265f354cd069cd3ef9bf286a28
[Skaut88] <[skaboy69@gmail.com]>
2023-05-23 16:44:15 +0300

    add css component, add state wiht users

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import { NavLink, Route, Routes } from "react-router-dom";
-import "./App.css";
+// import "./App.css";
 import { Profile } from "./components/Profile";
 import { Messages } from "./components/Message";
 import { Settings } from "./components/Settings";
 import { Shop } from "./components/Shop";
 
-function App() {
+function App(props) {
+  console.log(props);
   return (
     <div className="container mt-5">
       <div className="row mt-3">
@@ -27,15 +28,9 @@ function App() {
         </div>
         <div className="col-md-9">
           <Routes>
-            <Route path="/profile" element={<Profile />} />
-          </Routes>
-          <Routes>
-            <Route path="/messages" element={<Messages />} />
-          </Routes>
-          <Routes>
+            <Route path="/profile" element={<Profile function={props.function.key_getUser}/>} />
+            <Route path="/messages" element={<Messages function={props.function.key_getUsers}/>} />
             <Route path="/settings" element={<Settings />} />
-          </Routes>
-          <Routes>
             <Route path="/shop" element={<Shop />} />
           </Routes>
         </div>
--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -1,3 +1,37 @@
-export const Messages = () => {
-  return <h1>Эта страница с сообщениями</h1>;
+const TableRow = (props) => {
+  return (
+    <tr>
+      <th scope="row"></th>
+      <td>{props.lastname}</td>
+      <td>{props.name}</td>
+    </tr>
+  );
+};
+
+export const Messages = (props) => {
+  let users = props.function();
+  let usersCount = Object.keys(users).length;
+  let userRow = [];
+  for (let i = 0; i < usersCount; i++) {
+    userRow.push(<TableRow index={i} key={i} name={users[i].name} lastname={users[i].lastname} />)
+  }
+  return (
+    <>
+      <h2 className="text-center">Мои друзья</h2>
+      <div className="row">
+        <table className="table table-bordered">
+          <thead>
+            <tr>
+              <th scope="col">#</th>
+              <th scope="col">Фамилия</th>
+              <th scope="col">Имя</th>
+            </tr>
+          </thead>
+          <tbody>
+            {userRow}
+          </tbody>
+        </table>
+      </div>
+    </>
+  );
 };
