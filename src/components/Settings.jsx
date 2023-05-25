c4b8cd8cf5405500743070481ee6c830e5431d54
[Skaut88] <[skaboy69@gmail.com]>
2023-05-25 18:29:12 +0300

    add settings and edit code

--- src/components/Settings.jsx
+++ src/components/Settings.jsx
@@ -1,14 +1,20 @@
+import { useState } from "react";
+
+
 export const Settings = () => {
-  return (
-    <div className="mx-auto flex flex-col justify-between">
-      <div className="flex flex-col xl:flex-row md:text-base xl:text-xs">
-        <a href="tel:+71234567890" className="hover:text-red-800">
-          <span className="hidden xl:inline-block">
-            Moscow
-          </span>
-          +7(123)456-78-90
-        </a>
-      </div>
-    </div>
-  );
+	const [count, setCount] = useState(0);
+	return (
+<>
+<div className="row">
+	<div className="col-md-6">
+		<p>Количество кликов: {count}</p>
+	</div>
+	<div className="col-md-6">
+		<button className="btn btn-info" onClick={() => {
+			setCount(count + 1);
+		}}>Нажми на меня</button>
+	</div>
+</div>
+</>
+	);
 };
